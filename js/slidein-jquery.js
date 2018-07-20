9393117559dd6aabb9caacdb58e2d63106553080
Purvesh Makode <makodepurvesh@gmail.com>
2018-07-20 18:11:04 +0530

    slidein contact updated

--- js/slidein-jquery.js
+++ js/slidein-jquery.js
@@ -16,7 +16,7 @@ $(window).scroll(function() {
     var pos = $(this).offset().top;
 
     var winTop = $(window).scrollTop();
-    if (pos < winTop + 1000) {
+    if (pos < winTop + 900) {
       $(this).addClass("slide");
     }
   });
