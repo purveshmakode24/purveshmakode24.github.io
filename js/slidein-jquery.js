e67c83ad0e0270df4212da75a5be16af1ff40f4c
Purvesh Makode <makodepurvesh@gmail.com>
2018-07-20 15:58:19 +0530

    webpage updated

--- js/slidein-jquery.js
+++ js/slidein-jquery.js
@@ -4,8 +4,21 @@ $(window).scroll(function() {
     var pos = $(this).offset().top;
 
     var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
+    if (pos < winTop + 700 /*600*/) {
       $(this).addClass("slide");
     }
   });
 });
+
+/*
+$(window).scroll(function() {
+  $(".slideanim-1").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 700) {
+      $(this).addClass("slide");
+    }
+  });
+});*/
+
