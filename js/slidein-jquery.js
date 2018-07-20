bab2d70362039b3848d6b514791d28312404de7b
Purvesh Makode <makodepurvesh@gmail.com>
2018-07-20 16:13:31 +0530

    webpage updated

--- js/slidein-jquery.js
+++ js/slidein-jquery.js
@@ -10,15 +10,15 @@ $(window).scroll(function() {
   });
 });
 
-/*
+/* contact slide-in effect*/
 $(window).scroll(function() {
   $(".slideanim-1").each(function(){
     var pos = $(this).offset().top;
 
     var winTop = $(window).scrollTop();
-    if (pos < winTop + 700) {
+    if (pos < winTop + 900) {
       $(this).addClass("slide");
     }
   });
-});*/
+});
 
