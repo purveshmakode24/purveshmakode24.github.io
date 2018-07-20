09150c64e297f008dea7637b54712cea5adbe925
Purvesh Makode <makodepurvesh@gmail.com>
2018-07-20 16:18:06 +0530

    webpage updated

--- js/slidein-jquery.js
+++ js/slidein-jquery.js
@@ -16,7 +16,7 @@ $(window).scroll(function() {
     var pos = $(this).offset().top;
 
     var winTop = $(window).scrollTop();
-    if (pos < winTop + 900) {
+    if (pos < winTop + 1000) {
       $(this).addClass("slide");
     }
   });
